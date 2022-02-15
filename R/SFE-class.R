c3e424cf1046d0d358fe698c0b005378332de5c7
Lambda Moses <dlu2@caltech.edu>
2022-02-14 16:02:22 -0800

    Functions to convert an ordinary data frame to sf for *Geometry slots, as I have done in my exploratory analyses of the mouse muscle Visium dataset.

--- R/SFE-class.R
+++ R/SFE-class.R
@@ -61,36 +61,41 @@ setClass("SpatialFeatureExperiment",
 #'   polygons}{Also use \code{spatialCoordsNames}. There should an additional
 #'   column "ID" to specify which vertices belong to which polygon. The
 #'   coordinates should not be in list columns. Rather, the data frame should
-#'   look like it is passed to \code{ggplot2::geom_polygon}}. \item{Path to a
-#'   GeoJSON file}{For QuPath annotations.} } In all cases, the data frame
-#'   should specify the same number of geometries as the number of columns in
-#'   the gene count matrix. If the column "barcode" is present, then it will be
-#'   matched to column names of the gene count matrix. Otherwise, the geometries
-#'   are assumed to be in the same order as columns in the gene count matrix. If
-#'   the geometries are specified in an ordinary data frame, then it will be
-#'   converted into \code{sf} internally. Named list of data frames because each
-#'   entity can have multiple geometries, such as whole cell and nuclei
-#'   segmentations. The geometries are assumed to be POINTs for centroids and
-#'   POLYGONs for segmentations. If polygons are specified in an ordinary data
-#'   frame, then anything with fewer than 3 vertices will be removed. For anything
-#'   other than POINTs, attributes of the geometry will be ignored, but they can
-#'   be added later with the \code{AddGeometryAttr} function.
+#'   look like it is passed to \code{ggplot2::geom_polygon}. If there are holes,
+#'   then there must also be a column "subID" that differentiates between the
+#'   outer polygon and the holes.} \item{Path to a GeoJSON file}{For QuPath
+#'   annotations.} } In all cases, the data frame should specify the same number
+#'   of geometries as the number of columns in the gene count matrix. If the
+#'   column "barcode" is present, then it will be matched to column names of the
+#'   gene count matrix. Otherwise, the geometries are assumed to be in the same
+#'   order as columns in the gene count matrix. If the geometries are specified
+#'   in an ordinary data frame, then it will be converted into \code{sf}
+#'   internally. Named list of data frames because each entity can have multiple
+#'   geometries, such as whole cell and nuclei segmentations. The geometries are
+#'   assumed to be POINTs for centroids and POLYGONs for segmentations. If
+#'   polygons are specified in an ordinary data frame, then anything with fewer
+#'   than 3 vertices will be removed. For anything other than POINTs, attributes
+#'   of the geometry will be ignored, but they can be added later with the
+#'   \code{AddGeometryAttr} function.
 #' @param objectGeometry Geometry of objects other than those in
 #'   \code{primaryGeometry}, such as nuclei segmentation when the
 #'   \code{primaryGeometry} is Visium spots. It can be specified in the same
 #'   manner as \code{primaryGeometry}, as a named list. The data frames in the
 #'   list don't have to specify the same number of geometries, unlike in
 #'   \code{primaryGeometry}. The ordinary data frame must specify either POINTs
-#'   or POLYGONs. A warning is issued if an \code{objectGeometry} does not
-#'   overlap \code{primaryGeometry}.
+#'   or POLYGONs. A warning is issued if the bounding box of
+#'   \code{objectGeometry} does not overlap that of \code{primaryGeometry}.
 #' @param annotationGeometry Geometry of annotations of the tissue, such as
 #'   tissue boundary and pathologist annotations of histological regions. Also a
 #'   named list as in \code{primaryGeometry}. The ordinary data frame may
-#'   specify POINTs, POLYGONs, or LINESTRINGs. In the ordinary data frame, there
-#'   can be a column "geometry_type" specifying the geometry type. If that
-#'   column is absent, then any geometry with one vertice would be made a POINT,
-#'   any geometry with 2 vertices would be made a LINESTRING, and any geometry
-#'   with at least 3 vertices would be made a POLYGON.
+#'   specify POINTs, POLYGONs, or LINESTRINGs, or their MULTI versions. Each
+#'   data frame can only specify one type of geometry. For MULTI versions, there
+#'   must be a column "group" to identify each MULTI geometry.
+#' @param annotationGeometryType Character vector specifying geometry type of
+#'   each ordinary data frame in the list if \code{annotationGeometry} is
+#'   specified as such. Each element of the vector must be one of POINT,
+#'   LINESTRING, POLYGON, MULTIPOINT, MULTILINESTRING, and MULTIPOLYGON. Must be
+#'   the same length as the list of data frames.
 #' @param spotDiameter Spot diameter for technologies with arrays of spots of
 #'   fixed diameter per slide, such as Visium, ST, DBiT-seq, and slide-seq. The
 #'   diameter must be in the same unit as the coordinates in the *Geometry
@@ -101,7 +106,8 @@ setClass("SpatialFeatureExperiment",
 #' @param ... Additional arguments passed to the \code{\link{SpatialExperiment}}
 #'   and \code{\link{SingleCellExperiment}} constructors.
 #' @importFrom SpatialExperiment SpatialExperiment
-#' @importFrom sf st_point st_sfc
+#' @importFrom sf st_point st_sfc st_sf st_polygon st_buffer st_linestring st_multipoint
+#' st_multilinestring st_multipolygon
 #' @export
 SpatialFeatureExperiment <- function(assays, primaryGeometry,
                                      objectGeometry = list(object = make_empty_geometry()),
@@ -109,50 +115,11 @@ SpatialFeatureExperiment <- function(assays, primaryGeometry,
                                      colData = DataFrame(),
                                      rowData = NULL, spatialCoordsNames = c("x", "y"),
                                      sample_id = "sample01", spotDiameter = NA_real_,
+                                     annotationGeometryType = NA,
                                      unit = "full_res_image_pixels",
                                      ...) {
   # SCE will check the validity of assays
 
 }
 
-# From ordinary data frame to sf to construct SFE object
-.df2sf <- function(df, spatialCoordsNames, spotDiameter, allPolygons) {
-  if (any(!spatialCoordsNames %in% names(df))) {
-    cols_absent <- setdiff(spatialCoordsNames, names(df))
-    if (length(cols_absent) > 1L) {
-      stop("Columns ", paste(cols_absent, collapse = ", "), " are absent.")
-    } else {
-      stop("Column ", cols_absent, " is absent.")
-    }
-  }
-  if (!"ID" %in% names(df) || !anyDuplicated(df$ID)) {
-    # Case 1: centroids, use POINT
-    df$geometry <- lapply(seq_len(nrow(df)), function(i) {
-      st_point(c(df[[spatialCoordsNames[1]]], df[[spatialCoordsNames[2]]]))
-    })
-    df$geometry <- st_sfc(df$geometry)
-    df <- st_sf(df, sf_column_name = "geometry")
-  } else {
-    # Case 2: polygons
-    if (!"ID" %in% names(df)) {
-      stop("Column 'ID' must be present when the geometry is other than POINT.")
-    }
-    if (any(!names(df) %in% c("ID", spatialCoordsNames))) {
-      warning("Geometry attributes are ignored.")
-    }
-    n_vertices <- table(df$ID)
-    ids <- names(n_vertices)
-    if (allPolygons) {
-      ids_rm <- names(n_vertices[n_vertices < 3])
-      df <- df[!df$ID %in% ids_rm,]
-      if (!nrow(df)) {
-        stop("All geometries have 2 or fewer vertices. Cannot construct polygons.")
-      }
-    }
-    df_split <- split(df, df$ID)
-    geometry_use <- lapply(ids, function(i) {
-
-    })
-  }
-
-}
+# To do: unit test, validity, getters and setters
---
+++ R/df2sf.R
@@ -0,0 +1,141 @@
+.df2sf_check <- function(df, spatialCoordsNames, geometryType) {
+  # For anything other than points
+  if (!"ID" %in% names(df)) {
+    stop("Column 'ID' must be present when specifying ", geometryType, "s.")
+  }
+  if (grepl("MULTI", geometryType) && !"group" %in% names(df)) {
+    stop("Column 'group' must be present when specifying ", geometryType, "s.")
+  }
+  if (any(!names(df) %in% c("ID", spatialCoordsNames))) {
+    warning("Geometry attributes are ignored.")
+  }
+  n_vertices <- table(df$ID)
+  ids <- names(n_vertices)
+  min_vertices <- switch (geometryType,
+                          LINESTRING = 2L,
+                          POLYGON = 3L,
+                          MULTIPOINT = 1L,
+                          MULTILINESTRING = 2L,
+                          MULTIPOLYGON = 3L
+  )
+  ids_rm <- names(n_vertices[n_vertices < min_vertices])
+  df <- df[!df$ID %in% ids_rm,]
+  if (!nrow(df)) {
+    stop("All geometries have fewer than ", min_vertices, " vertices. ",
+         "Cannot construct ", geometryType)
+  }
+}
+
+.df2sf_point <- function(df, spatialCoordsNames, spotDiameter, multi) {
+  # Case 1: centroids, use POINT
+  if (!is.na(spotDiameter)) {
+    if (spotDiameter <= 0) {
+      stop("spotDiameter must be a positive number.")
+    }
+  }
+  if (multi) {
+    .df2sf_check(df, spatialCoordsNames, "MULTIPOINT")
+    df_split <- split(df, df$group)
+    geometry_use <- lapply(df_split, function(x) {
+      st_multipoint(as.matrix(x[,spatialCoordsNames]))
+    })
+    geometry_use <- st_sfc(geometry_use)
+    out <- st_sf(group = names(df_split), geometry = geometry_use)
+  } else {
+    df$geometry <- lapply(seq_len(nrow(df)), function(i) {
+      st_point(c(df[[spatialCoordsNames[1]]], df[[spatialCoordsNames[2]]]))
+    })
+    df$geometry <- st_sfc(df$geometry)
+    out <- st_sf(df, sf_column_name = "geometry")
+  }
+  if (!is.na(spotDiameter)) {
+    out$geometry <- st_buffer(out$geometry, spotDiameter/2)
+  }
+  out
+}
+
+.df2poly_mat <- function(x, spatialCoordsNames) {
+  m <- as.matrix(x[,spatialCoordsNames])
+  # Close the polygon
+  rbind(m, m[1,])
+}
+.df2sf_polygon <- function(df, spatialCoordsNames, multi) {
+  df <- unique(df)
+  gt <- if (multi) "MULTIPOLYGON" else "POLYGON"
+  .df2sf_check(df, spatialCoordsNames, gt)
+  if (multi) {
+    df_split <- split(df, df$group)
+    geometry_use <- lapply(df_split, function(x) {
+      ms1 <- split(x, x$ID)
+      if ("subID" %in% names(df)) {
+        m <- lapply(ms1, function(y) {
+          ms2 <- split(y, y$subID)
+          lapply(ms2, .df2poly_mat, spatialCoordsNames = spatialCoordsNames)
+        })
+      } else {
+        m <- lapply(ms1, function(y) list(.df2poly_mat(y, spatialCoordsNames)))
+      }
+      st_multipolygon(m)
+    })
+  } else {
+    df_split <- split(df, df$ID)
+    if ("subID" %in% names(df)) {
+      # Might be holes
+      geometry_use <- lapply(df_split, function(y) {
+        ms <- split(y, y$subID)
+        out <- lapply(ms, .df2poly_mat, spatialCoordsNames = spatialCoordsNames)
+        st_polygon(out)
+      })
+    } else { # Definitely no holes
+      geometry_use <- lapply(df_split, function(y) {
+        st_polygon(list(.df2poly_mat(y, spatialCoordsNames)))
+      })
+    }
+  }
+  geometry_use <- st_sfc(geometry_use)
+  st_sf(ID = names(df_split), geometry = geometry_use, crs = NA)
+}
+
+.df2sf_linestring <- function(df, spatialCoordsNames, multi) {
+  df <- unique(df)
+  gt <- if (multi) "MULTILINESTRING" else "LINESTRING"
+  .df2sf_check(df, spatialCoordsNames, gt)
+  if (multi) {
+    df_split <- split(df, df$group)
+    geometry_use <- lapply(df_split, function(x) {
+      ms <- split(x, x$ID)
+      m <- lapply(ms, function(y) as.matrix(y[,spatialCoordsNames]))
+      st_multilinestring(m)
+    })
+  } else {
+    df_split <- split(df, df$ID)
+    geometry_use <- lapply(df_split, function(x) {
+      st_linestring(as.matrix(x[,spatialCoordsNames]))
+    })
+  }
+  geometry_use <- st_sfc(geometry_use)
+  st_sf(ID = names(df_split), geometry = geometry_use, crs = NA)
+}
+
+# From ordinary data frame to sf to construct SFE object
+.df2sf <- function(df, spatialCoordsNames, spotDiameter, geometryType) {
+  if (any(!spatialCoordsNames %in% names(df))) {
+    cols_absent <- setdiff(spatialCoordsNames, names(df))
+    if (length(cols_absent) > 1L) {
+      stop("Columns ", paste(cols_absent, collapse = ", "), " are absent.")
+    } else {
+      stop("Column ", cols_absent, " is absent.")
+    }
+  }
+  is_de_facto_point <- (!"ID" %in% names(df) | !anyDuplicated(df$ID)) &
+    !"group" %in% names(df)
+  if (is_de_facto_point) geometryType <- "POINT"
+  switch (geometryType,
+          POINT = .df2sf_point(df, spatialCoordNames, spotDiameter, multi = FALSE),
+          MULTIPOINT = .df2sf_point(df, spatialCoordNames, spotDiameter, multi = TRUE),
+          LINESTRING = .df2sf_linestring(df, spatialCoordNames, multi = FALSE),
+          MULTILINESTRING = .df2sf_linestring(df, spatialCoordNames, multi = TRUE),
+          POLYGON = .df2sf_polygon(df, spatialCoordNames, multi = FALSE),
+          MULTIPOLYGON = .df2sf_polygon(df, spatialCoordNames, multi = TRUE)
+  )
+}
