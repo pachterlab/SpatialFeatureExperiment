b09479712df18c53c723cb6fff9306d2982a1c32
Lambda Moses <dlu2@caltech.edu>
2022-02-28 16:13:17 -0800

    Get rid of false positives in validity

--- R/validity.R
+++ R/validity.R
@@ -4,32 +4,33 @@
 .valid_geometry <- function(g, name_show) {
   # Make message if something in an element of *Geometries is not valid
   v <- st_is_valid(g, reason = TRUE)
-  which_invalid <- which(v != "ValidGeometry")
+  which_invalid <- which(v != "Valid Geometry")
   if (length(which_invalid)) {
     paste0("The following items (index shown) in ", name_show, " are invalid:\n",
            paste0(which_invalid, ": ", v[which_invalid], collapse = "\n"))
   } else {
-    return("")
+    return(character(0))
   }
 }
 .check_geometries <- function(gs, name_show) {
-  msgs <- vapply(seq_along(gs), function(i) {
+  msgs <- lapply(seq_along(gs), function(i) {
     if (is(gs[[i]], "sf")) {
       m <- .valid_geometry(gs[[i]], names(gs)[i])
     } else {
       paste0("Item ", i, " in ", name_show, " is ", class(gs[[i]])[1], " rather than sf.\n")
     }
-  }, FUN.VALUE = character(1))
+  })
+  msgs <- unlist(msgs)
   if (length(msgs)) {
     paste0("Issues in ", name_show, ":\n", paste(msgs, collapse = "\n"), "\n")
   } else {
     return(character(0))
   }
 }
-setValidity("SpatialFeatureExperiment", function(x) {
-  col_msg <- .check_geometries(int_colData(x)$colGeometries, "colGeometries")
-  row_msg <- .check_geometries(int_elementMetadata(x)$rowGeometries, "rowGeometries")
-  annot_msg <- .check_geometries(int_metadata(x)$annotGeometries, "annotGeometries")
+setValidity("SpatialFeatureExperiment", function(object) {
+  col_msg <- .check_geometries(int_colData(object)$colGeometries, "colGeometries")
+  row_msg <- .check_geometries(int_elementMetadata(object)$rowGeometries, "rowGeometries")
+  annot_msg <- .check_geometries(int_metadata(object)$annotGeometries, "annotGeometries")
   outs <- c(col_msg, row_msg, annot_msg)
   if (length(outs)) {
     return(outs)
