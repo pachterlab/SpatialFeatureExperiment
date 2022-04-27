4d32877564e8b51167bf4a183802b981380ce025
Lambda Moses <dlu2@caltech.edu>
2022-04-26 19:47:43 -0700

    Remove make_empty_geometry; it's no longer used anywhere in this package.

--- R/utils.R
+++ R/utils.R
@@ -1,14 +1,3 @@
-#' Make empty geometry
-#'
-#' For default arguments in some functions with the right type.
-#'
-#' @return An \code{sf} data frame whose geometry is an empty GEOMETRYCOLLECTION.
-#' @importFrom sf st_sf st_sfc st_geometrycollection
-#' @export
-make_empty_geometry <- function() {
-  st_sf(st_sfc(st_geometrycollection()))
-}
-
 .getfun <- function(MARGIN) switch (MARGIN, int_elementMetadata, int_colData)
 
 .setfun <- function(MARGIN) switch (MARGIN, `int_elementMetadata<-`, `int_colData<-`)
