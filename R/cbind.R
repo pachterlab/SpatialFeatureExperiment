0c17b3438e68a1cff573b220c7835cdb5365c7d3
Lambda Moses <dlu2@caltech.edu>
2024-02-20 18:47:13 -0800

    Updated cbind to deal with rowGeometries (more complicated than I thought)

--- R/cbind.R
+++ R/cbind.R
@@ -11,20 +11,20 @@
     if (length(names_intersect)) {
         # A bit of repetition, just to get the names right
         which_rbind <- vapply(names_intersect, function(n) {
-            ag_x <- annotGeometry(x, n)
-            ag_y <- annotGeometry(y, n)
+            ag_x <- annotGeometry(x, n, sample_id = "all")
+            ag_y <- annotGeometry(y, n, sample_id = "all")
             setequal(names(ag_x), names(ag_y))
         }, FUN.VALUE = logical(1))
 
         out_rbind <- lapply(names_intersect[which_rbind], function(n) {
-            ag_x <- annotGeometry(x, n)
-            ag_y <- annotGeometry(y, n)
+            ag_x <- annotGeometry(x, n, sample_id = "all")
+            ag_y <- annotGeometry(y, n, sample_id = "all")
             ag_y <- ag_y[, names(ag_x)]
             rbind(ag_x, ag_y)
         })
         out_append <- lapply(names_intersect[!which_rbind], function(n) {
-            ag_x <- annotGeometry(x, n)
-            ag_y <- annotGeometry(y, n)
+            ag_x <- annotGeometry(x, n, sample_id = "all")
+            ag_y <- annotGeometry(y, n, sample_id = "all")
             setNames(list(ag_x, ag_y), paste(n,
                 c(ag_x$sample_id[1], ag_y$sample_id[1]),
                 sep = "_"
@@ -41,108 +41,6 @@
     }
     out <- c(out_rbind, out_append, out_x, out_y)
     return(out)
-}
-
-.rg_sample_rename <- function(nms, sample_id) {
-    if (is.null(nms)) return(NULL)
-    # Append sample_id to items that don't already have sample_id
-    # For one sample
-    inds <- !grepl(paste0(sample_id, "$"), nms)
-    nms[inds] <- paste(nms[inds], sample_id, sep = "_")
-    nms
-}
-
-.rg_xy_rename <- function(nms, sample_id, overlap, xy) {
-    if (is.null(nms)) return(NULL)
-    inds <- grepl(paste0(sample_id, "$"), nms)
-    nms[inds] <- str_remove(nms[inds], paste0("_", sample_id, "$"))
-    which_overlap <- which(nms %in% overlap)
-    nms[which_overlap] <- paste(nms[which_overlap], xy, sep = "_")
-    nms <- paste(nms, sample_id, sep = "_")
-    nms
-}
-
-.has_any_sample <- function(nms, sample_ids) {
-    patterns <- paste0(sample_ids, "$")
-    out <- vapply(patterns, grepl, x = nms, FUN.VALUE = logical(length(nms)))
-    # out is a matrix with patterns as columns
-    apply(out, 1, any)
-}
-
-.add_xy <- function(rgns, sample_ids, xy) {
-    pattern <- paste0("_", paste0(sample_ids, collapse = "|"), "$")
-    base <- str_remove(rgns, pattern)
-    samples <- str_extract(rgns, pattern)
-    samples[is.na(samples)] <- ""
-    paste0(base, "_", xy, samples)
-}
-
-.disambiguate <- function(rgns, samples, other, xy) {
-    if (is.null(rgns)) return(NULL)
-    has_samples <- .has_any_sample(rgns, samples)
-    to_add_x <- has_samples & rgns %in% other
-    rgns[to_add_x] <- .add_xy(rgn1[to_add_x], samples, xy)
-    rgns
-}
-
-.rg_xy_multi <- function(rgn1, rgn2, samples1, samples2) {
-    # a. items that overlap, with the same sample_ids, add x or y
-    samples <- intersect(samples1, samples2)
-    rgn1 <- .disambiguate(rgn1, samples, rgn2, "x")
-    rgn2 <- .disambiguate(rgn2, samples, rgn1, "y")
-    # b. items with different sample_ids, no need to change
-    list(x = rgn1, y = rgn2)
-}
-
-.combine_rowgeos <- function(x, y) {
-    rg_names_x <- rowGeometryNames(x)
-    rg_names_y <- rowGeometryNames(y)
-    if (is.null(rg_names_x) && is.null(rg_names_y))
-        return(NULL)
-
-    samples_x <- sampleIDs(x)
-    samples_y <- sampleIDs(y)
-    # Scenarios:
-    # 1. x and y each has one sample, with different sample_ids. In this case
-    # I'll simply append the sample_id if not already present
-    if (length(samples_x) == 1L && length(samples_y) == 1L) {
-        if (samples_x != samples_y) {
-            rg_names_x <- .rg_sample_rename(rg_names_x, samples_x)
-            rg_names_y <- .rg_sample_rename(rg_names_y, samples_y)
-        } else {
-            # 2. x and y have different sample_ids and rowGeometry with the same name
-            # What to do? Maybe I'll just append "y" on the one from y. Sometimes they're
-            # meant to be the same sample but sometimes they're not and the user forgot
-            # to change it. What to do?
-            # Don't want to break my rule to put sample_id by the end of the name
-            # So it will become name_x/y_sample
-            overlap <- intersect(rg_names_x, rg_names_y)
-            rg_names_x <- .rg_xy_rename(rg_names_x, samples_x, overlap, "x")
-            rg_names_y <- .rg_xy_rename(rg_names_y, samples_y, overlap, "y")
-        }
-    } else {
-        # 3. At least one of x and y have multiple samples, some of which are for all
-        # samples in the object and some are sample-specific. What to do? For those
-        # that are not sample-specific or have overlapping names in x and y, I'll
-        # append x or y to the name.
-        new_rgns <- .rg_xy_multi(rg_names_x, rg_names_y, samples_x, samples_y)
-        rg_names_x <- new_rgns[[1]]
-        rg_names_y <- new_rgns[[2]]
-
-        # Then what if we split the SFE object to form two samples? Might not need to split rowGeometries?
-        # Let me not deal with this case (1 and 2 here) right now, until I implement the tree.
-        # 1. items that overlap, without sample_id, i.e. for all samples in the object
-        # This system of _sample needs to generalize when I implement the tree of sample relations
-        # 2. items that don't overlap, without sample_id => now what, it's not sample specific but doesn't apply for the entire SFE object?
-        # So that's connected to the tree.
-    }
-    rgsx <- rowGeometries(x)
-    rgsy <- rowGeometries(y)
-
-    names(rgsx) <- rg_names_x
-    names(rgsy) <- rg_names_y
-    return(c(rgsx, rgsy))
-    # Output: new list of the combined rowGeometries
 }
 
 #' Concatenate SpatialFeatureExperiment objects
@@ -165,23 +63,87 @@
 setMethod(
     "cbind", "SpatialFeatureExperiment",
     function(..., deparse.level = 1) {
+        args <- list(...)
+        if (length(args) < 2L) return(args[[1]]) # just like cbind for matrix
         old <- S4Vectors:::disableValidity()
         if (!isTRUE(old)) {
             S4Vectors:::disableValidity(TRUE)
             on.exit(S4Vectors:::disableValidity(old))
         }
-        args <- list(...)
-        new_rg <- Reduce(.combine_rowgeos, args)
-        args <- lapply(args, function(x) {
-            rowGeometries(x) <- NULL
-            x
-        })
+        # Like in SPE, make sure sample_ids are unique
+        sids_list <- lapply(args, sampleIDs)
+        sids <- unlist(sids_list)
+        sid_df <- data.frame(id = sids,
+                             object = rep(seq_along(sids_list), times = lengths(sids_list)))
+        if (length(sids) > length(unique(sids))) {
+            message(
+                "'sample_id's are duplicated across",
+                " 'SpatialFeatureExperiment' objects to cbind;",
+                " appending sample indices.")
+            # Might not be fast but since I only expect a small number of SFE
+            # objects it should be fine
+            for (i in seq_along(args)) {
+                ind <- sid_df$object < i
+                for (s in sids_list[[i]]) {
+                    n_existing <- sum(s == sid_df$id[ind])
+                    if (n_existing == 0L) next
+                    id_new <- paste(s, n_existing, sep = "_")
+                    names(id_new) <- s
+                    args[[i]] <- changeSampleIDs(args[[i]], id_new)
+                }
+            }
+        }
+        # Rename rowGeometries that are not sample specific
+        for (i in seq_along(args)) {
+            if (length(sids_list[[i]]) == 1L) {
+                rgns <- rowGeometryNames(args[[i]])
+                if (is.null(rgns)) next
+                s <- sampleIDs(args[[i]])
+                to_add <- !grepl(paste0(s, "$"), rgns)
+                rgns[to_add] <- paste(rgns[to_add], s, sep = "_")
+                rowGeometryNames(args[[i]]) <- rgns
+            }
+        }
+        # Multiple samples, with rowGeometries that are not sample specific
+        rgns_list <- lapply(args, rowGeometryNames)
+        rgns <- unlist(rgns_list)
+        if (length(rgns) > length(unique(rgns))) {
+            # Presumably some of the SFE objects have rowGeometries
+            rgn_df <- data.frame(id = rgns,
+                                 object = rep(seq_along(rgns_list), times = lengths(rgns_list)))
+            for (i in seq_along(args)) {
+                if (is.null(rgns_list[[i]])) next
+                ind <- rgn_df$object < i
+                rgns_use <- rgns_list[[i]]
+                rgns_old <- rgns_use
+                for (j in seq_along(rgns_use)) {
+                    s <- rgns_use[j]
+                    n_existing <- sum(s == rgn_df$id[ind])
+                    if (n_existing == 0L) next
+                    rgns_use[j] <- paste(s, n_existing, sep = "_")
+                    # This is tentative. I'll implement the tree thing to make it clearer
+                }
+                if (!identical(rgns_use, rgns_old))
+                    rowGeometryNames(args[[i]]) <- rgns_use
+            }
+        }
+        # Still need to combine rowGeometries separately
+        if (length(rgns)) {
+            rgs <- lapply(args, rowGeometries)
+            rgs <- do.call(c, rgs)
+            args <- lapply(args, function(x) {
+                rowGeometries(x) <- NULL
+                x
+            })
+        }
         out <- do.call(
             callNextMethod,
             c(args, list(deparse.level = deparse.level))
         )
         colnames(out) <- make.unique(colnames(out), sep = "-")
-        rowGeometries(out) <- new_rg
+        if (length(rgns)) {
+            rowGeometries(out) <- rgs
+        }
         # Combine the annotGeometries
         has_ag <- vapply(args, function(a) length(annotGeometries(a)) > 0L,
             FUN.VALUE = logical(1)
--- tests/testthat/test-cbind.R
+++ tests/testthat/test-cbind.R
@@ -25,3 +25,62 @@ test_that("cbind properly deals with annotGeometries and spatialGraphs", {
     expect_equal(ag_orig, ag_concat)
     expect_equal(spatialGraphs(sfe_visium), spatialGraphs(sfe_concat))
 })
+
+# With rowGeometries
+cg_toy <- readRDS(system.file("extdata/cg_toy.rds",
+                              package = "SpatialFeatureExperiment"
+))
+cg_toy2 <- readRDS(system.file("extdata/cg_toy2.rds",
+                               package = "SpatialFeatureExperiment"
+))
+rg_toy1 <- cg_toy[1:2,]
+rownames(rg_toy1) <- rownames(sfe_visium)
+rg_toy2 <- cg_toy[3:4,]
+rownames(rg_toy2) <- rownames(sfe_visium)
+rg_toy3 <- cg_toy[4:5,]
+rownames(rg_toy3) <- rownames(sfe_visium)
+
+test_that("Deal with duplicate sample_id", {
+    sfe_visium2 <- changeSampleIDs(sfe_visium2, c(sample02 = "sample01"))
+    # I need to check what's so slow in cbind but not the most urgent
+    expect_message(sfe2 <- cbind(sfe_visium1, sfe_visium2),
+                   "'sample_id's are duplicated across")
+    expect_setequal(sampleIDs(sfe2), c("sample01", "sample01_1"))
+})
+
+test_that("rowGeometry, both x and y has 1 sample, rowGeometry names don't have samples", {
+    txSpots(sfe_visium1) <- rg_toy1
+    txSpots(sfe_visium2) <- rg_toy2
+    sfe2 <- cbind(sfe_visium1, sfe_visium2)
+    expect_equal(rowGeometryNames(sfe2), c("txSpots_sample01", "txSpots_sample02"))
+    expect_equal(txSpots(sfe2, "sample01"), rg_toy1)
+    expect_equal(txSpots(sfe2, "sample02"), rg_toy2)
+})
+
+test_that("rowGeometry, multiple samples", {
+    txSpots(sfe_visium, "sample01") <- rg_toy1
+    txSpots(sfe_visium, "sample02") <- rg_toy2
+    txSpots(sfe_visium, "all") <- rg_toy3
+    sfe_visiumb <- sfe_visium
+    sfe_visiumb <- changeSampleIDs(sfe_visiumb, c(sample01 = "foo", sample02 = "bar"))
+    sfe2 <- cbind(sfe_visium, sfe_visiumb)
+    expect_equal(rowGeometryNames(sfe2),
+                 c("txSpots_sample01", "txSpots_sample02", "txSpots", "txSpots_foo",
+                   "txSpots_bar", "txSpots_1"))
+    expect_equal(txSpots(sfe2, "sample01"), rg_toy1)
+    expect_equal(txSpots(sfe2, "all"), rg_toy3)
+    expect_equal(txSpots(sfe2, "bar"), rg_toy2)
+})
+
+test_that("What if some but not all of the objects don't have rowGeometries", {
+    txSpots(sfe_visium1) <- rg_toy1
+    sfe2 <- cbind(sfe_visium1, sfe_visium2)
+    expect_equal(rowGeometryNames(sfe2), "txSpots_sample01")
+    expect_equal(txSpots(sfe2, "sample01"), rg_toy1)
+})
+
+test_that("cbind when only one or no SFE object is specified", {
+    expect_null(SpatialFeatureExperiment::cbind())
+    sfe2 <- cbind(sfe_visium1)
+    expect_equal(sfe2, sfe_visium1)
+})
