d6a139b4c49cf1468e316803a0fb5d485fee2842
Andrew Newman <andrewfnewman@gmail.com>
2024-07-18 19:36:54 +1000

    Add v1-v5 barcodes and keep existing.

--- inst/script/visium_row_col.R
+++ inst/script/visium_row_col.R
@@ -1,5 +1,22 @@
 # I installed Space Ranger in my home directory
-visium_row_col <- read.delim("~/spaceranger-1.3.1/lib/python/cellranger/barcodes/visium-v1_coordinates.txt",
-                             header = FALSE,
-                             col.names = c("barcode", "col", "row"))
+spacerange_dir <- "~/tmp/spaceranger-3.0.1/lib/python/cellranger/barcodes"
+
+get_data <- function(file_path) {
+    data <- read.delim(file_path, header = FALSE, col.names = c("barcode", "col", "row"))
+    return(data)
+}
+
+visium_row_col <- get_data(paste0(spacerange_dir, "/visium-v1_coordinates.txt"))
+visium_row_col_v1 <- get_data(paste0(spacerange_dir,"/visium-v1_coordinates.txt"))
+visium_row_col_v2 <- get_data(paste0(spacerange_dir,"/visium-v2_coordinates.txt"))
+visium_row_col_v3 <- get_data(paste0(spacerange_dir,"/visium-v3_coordinates.txt"))
+visium_row_col_v4 <- get_data(paste0(spacerange_dir,"/visium-v4_coordinates.txt"))
+visium_row_col_v5 <- get_data(paste0(spacerange_dir,"/visium-v5_coordinates.txt"))
+
 usethis::use_data(visium_row_col, overwrite = TRUE)
+usethis::use_data(visium_row_col_v1, overwrite = TRUE)
+usethis::use_data(visium_row_col_v2, overwrite = TRUE)
+usethis::use_data(visium_row_col_v3, overwrite = TRUE)
+usethis::use_data(visium_row_col_v4, overwrite = TRUE)
+usethis::use_data(visium_row_col_v5, overwrite = TRUE)
+
