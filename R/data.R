b41fd6368411b7fecf7cce154290ad6e6b056a22
Andrew Newman <andrewfnewman@gmail.com>
2024-07-23 11:24:53 +1000

    Update documentation.

--- R/data.R
+++ R/data.R
@@ -1,8 +1,53 @@
-#' Row and columns of Visium barcodes on the slide
+#' Row and columns of Visium V1 barcodes on the slide
 #'
-#' From Space Ranger 1.3.1.
+#' From Space Ranger 3.0.1.
 #'
 #' @format A data frame with 4992 rows with columns barcode, col, and row.
 #' @concept Utilities
-#' @source Space Ranger 1.3.1
+#' @source Space Ranger 3.0.1.
 "visium_row_col"
+
+#' Row and columns of Visium V1 barcodes on the slide
+#'
+#' From Space Ranger 3.0.1.
+#'
+#' @format A data frame with 4992 rows with columns barcode, col, and row.
+#' @concept Utilities
+#' @source Space Ranger 3.0.1.
+"visium_row_col_v1"
+
+#' Row and columns of Visium V2 barcodes on the slide
+#'
+#' From Space Ranger 3.0.1.
+#'
+#' @format A data frame with 4992 rows with columns barcode, col, and row.
+#' @concept Utilities
+#' @source Space Ranger 3.0.1.
+"visium_row_col_v2"
+
+#' Row and columns of Visium V3 barcodes on the slide
+#'
+#' From Space Ranger 3.0.1.
+#'
+#' @format A data frame with 4992 rows with columns barcode, col, and row.
+#' @concept Utilities
+#' @source Space Ranger 3.0.1.
+"visium_row_col_v3"
+
+#' Row and columns of Visium V4 barcodes on the slide
+#'
+#' From Space Ranger 3.0.1.
+#'
+#' @format A data frame with 4992 rows with columns barcode, col, and row.
+#' @concept Utilities
+#' @source Space Ranger 3.0.1.
+"visium_row_col_v4"
+
+#' Row and columns of Visium V5 barcodes on the slide
+#'
+#' From Space Ranger 3.0.1.
+#'
+#' @format A data frame with 14336 rows with columns barcode, col, and row.
+#' @concept Utilities
+#' @source Space Ranger 3.0.1.
+"visium_row_col_v5"
