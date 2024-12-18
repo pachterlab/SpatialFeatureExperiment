d43871bb77504da3913621cac9a40425147066a1
Eva C. Freckmann <efreckmann@yahoo.com>
2024-12-18 14:23:45 +0000

    Update read.R
    Updated readCosMX() to handle if both compressed and uncompressed CosMX files are present

--- R/read.R
+++ R/read.R
@@ -987,9 +987,9 @@ readCosMX <- function(data_dir,
     check_installed("sfarrow")
     data_dir <- normalizePath(data_dir, mustWork = TRUE)
     fns <- list.files(data_dir, pattern = "\\.csv.*$", full.names = TRUE)
-    fn_metadata <- grep("metadata", fns, value = TRUE)
-    fn_mat <- grep("exprMat", fns, value = TRUE)
-    fn_polys <- grep("polygons", fns, value = TRUE)
+    fn_metadata <- grep("metadata", fns, value = TRUE)[1]
+    fn_mat <- grep("exprMat", fns, value = TRUE)[1]
+    fn_polys <- grep("polygons", fns, value = TRUE)[1]
 
     meta <- fread(fn_metadata)
     mat <- fread(fn_mat) # TODO: write to h5 or mtx. Consult alabaster.sce
